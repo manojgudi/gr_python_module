2c085e75047d76b56599ab6d1a203feca57f54b7
manojgudi <manoj.p.gudi@gmail.com>
2013-08-21 16:42:29 +0530

    input_function -> user_specific by Aviral Chandra <aviral.chandra@gmail.com>

--- scilab_func.py
+++ scilab_func.py
@@ -1,17 +1,19 @@
+#!/usr/bin/python -tt
+
 
 def calc_op(n0,n1,n2,  d0,d1,d2):
 	string1 = "s=%s; h=syslin('c',"
 	string2 = str(n0)+"*s^2+"+str(n1)+"*s+"+str(n2)+","
 	string3 = str(d0)+"*s^2+"+str(d1)+"*s+"+str(d2)+");"
-	string4 = "t=0:0.01:10; r=tf2ss(h); a=csim('step',t,r)"
+	string4 = "t=0:0.01:10;"
+	string5 = "deff('u=input(t)','u=abs(sin(t))');"
+	string6 = "r=tf2ss(h); a=csim(input,t,r);"
 
-	full_string = string1+string2+string3+string4
+	full_string = string1+string2+string3+string4+string5+string6
 	
 	print "ex:"
 
 	print full_string
-
-	print "s=%s;h=syslin('c',s+1,s^2+s+1);t=0:0.01:10;r=tf2ss(h),a=csim('step',t,r)"
 
 	import sciscipy
 	sciscipy.eval(full_string)
@@ -21,4 +23,4 @@ def calc_op(n0,n1,n2,  d0,d1,d2):
 	return b
 
 
-print calc_op(1,2,0, 0,3,1)
+print calc_op(1,2,4, 2,3,1)
