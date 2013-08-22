8fa0bf5757be84c804599c6cfba0dcf5f92dd34c
aviral <aviral.chandra1703@gmail.com>
2013-08-22 13:13:24 +0530

    discrete time response with user input and user specified transfer function

--- disc_simul_scilab.py
+++ disc_simul_scilab.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
 import sciscipy
-sciscipy.eval("s=%s;h=syslin(0.5,s+1,s+5);r=tf2ss(h);u=zeros(1,50);u(10)=1;y=dsimul(r,u);a=size(y)//;disp(a)")
-y=sciscipy.read("y")
-a=sciscipy.read("a")
-print y
-print a
+import matplotlib.pyplot as plt
+
+def discrete_sim(n0,n1,st,d0,d1,u):
+        string1 = "s=%s;"
+        string2 = "h=syslin("
+        string3 = str(st)+","+str(n0)+"*s"+"+"+str(n1)+","+str(d0)+"*s"+"+"+str(d1)+");"
+        string4 = "r=tf2ss(h);"
+        string5 = u+";"
+        string6 = "y=dsimul(r,"u")"
+        string = string1+string2+string3+string4+string5+string6
+	print string
+	import sciscipy
+	sciscipy.eval(string)
+	y = sciscipy.read("y")
+	return y
+
+print discrete_sim(1,1,0.1,2,1,"u=zeros(1,50);u(10)=1")
+
