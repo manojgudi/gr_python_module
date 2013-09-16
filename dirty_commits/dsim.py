7bfbb5622f1e44262a045baaeb961816ec093632
manojgudi <manoj.p.gudi@gmail.com>
2013-09-16 17:23:21 +0530

    Oh Gods of git I ask for Thy forgiveness for I have made the sin of committing irrelevant files in a separate directory

---
+++ dirty_commits/dsim.py
@@ -0,0 +1,30 @@
+import gras
+import numpy
+
+class dsim(gras.Block):
+
+	def __init__(self):
+		gras.Block.__init__(self,
+			name="dsim",
+			in_sig=[numpy.float32],
+			out_sig=[numpy.float32])
+
+
+	def work(self, input_items, output_items):
+		
+		#n = min(len(input_items[0]), len(output_items[0]))
+		n=1
+		in0 = input_items[0]
+		out = output_items[0]
+		
+		from dsim_sci import discrete_sim
+		#Processing 
+		# Assuming n = 1 input_config(0)=1
+		
+		out[:n] = discrete_sim(2,3,0.5,1,2,str(in0[:n][0]))
+		
+		print out[:n], in0[:n]
+
+		self.consume(0,n) # Consume from port 0 input_items
+		self.consume(1,n) # Consume from port 1 input_items
+		self.produce(0,n) # Produce from port 0 output_items
---
+++ dirty_commits/dsim_sci.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sciscipy
+import matplotlib.pyplot as plt
+
+def discrete_sim(n0,n1,st,d0,d1,u):
+        string1 = "s=%s;"
+        string2 = "h=syslin("
+        string3 = str(st)+","+str(n0)+"*s"+"+"+str(n1)+","+str(d0)+"*s"+"+"+str(d1)+");"
+        string4 = "r=tf2ss(h);"
+        string5 = "u="+str(u)+";"
+        string6 = "y=dsimul(r,u)"
+        string = string1+string2+string3+string4+string5+string6
+
+	# Check complete_string 
+	#print string
+
+	import sciscipy
+	sciscipy.eval(string)
+	y = sciscipy.read("y")
+	return y
+
+#print discrete_sim(1,1,0.1,2,1,"u=zeros(1,50);u(10)=1")
+
