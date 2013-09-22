7aedc44bd75a8b25a0c55a11eec74d9e95bd0d82
manojgudi <manoj.p.gudi@gmail.com>
2013-09-22 22:25:27 +0530

    correctly handled weird numpy print | handled incorrect non integral window size

--- dirty_commits/generic/generic.py
+++ dirty_commits/generic/generic.py
@@ -12,12 +12,15 @@ class generic(gras.Block):
 	def set_parameters(self,func_name,window):
 		self.n = window
 		self.func_name = func_name
-
+	
+	# Check if value of window is integral of length of input source vector 
+	# For cases like -> input = [3 , 4, 5 ,6] & window = 3
+	def isIntegralWin(self, input_item, window):
+		if (len(input_item) % window ):
+			raise Exception("Value of Window should be integral of length of input items")
+			
 		
 	def work(self, input_items, output_items):
-		
-		#self.n =1 
-		#n = min(len(input_items[0]), len(output_items[0]))
 		
 		from scilab import Scilab
 		Sci = Scilab()
@@ -32,22 +35,22 @@ class generic(gras.Block):
 		out_eval_string = 'eval("Sci.'+self.func_name+'('
 		in_dic = {}
 		for i in range(n_input_items):
+			
+			self.isIntegralWin(input_items[i][:], self.n)
+
 			if self.n == 1:
 				out_eval_string = out_eval_string + str(input_items[i][:self.n]) + ","
-		# If the window is greater than 1,  input_items[i][:self.n] looks like [1   2   3   4   5] which is err for python since it requires comma as delimiters 
+	
+			# If the window is greater than 1,  input_items[i][:self.n] looks like [1   2   3   4   5] which is err for python since it requires comma as delimiters 
 			else:	
-				print str(input_items[i][:self.n])
-				out_eval_string = out_eval_string + (str(input_items[i][:self.n]).tolist())  + ","  # Replace 10spaces with a singe comma
-				
+				print 'IN',str(input_items[i][:self.n])
+				out_eval_string = out_eval_string + (str(input_items[i][:self.n].tolist()))  + ","  # Replace 10spaces with a singe comma
+						
 		out_eval_string = out_eval_string.rstrip(",") + ')")'
-		print "OUT",str(out_eval_string)
+		print "STRING",str(out_eval_string)
 		out = eval(out_eval_string)
-		#out = eval("Sci."+self.func_name+"("+str(in0[:self.n])+","+str(in1[:self.n])+")")
 		
-		#in0 = input_items[0]
-		#in1 = input_items[1]
-		
-		print out
+		print "OUT",out
 		
 		# Write a for loop for n_inputs
 		for i in range(n_input_items):
