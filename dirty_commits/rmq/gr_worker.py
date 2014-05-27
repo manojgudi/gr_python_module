0856e00ce7be5f0eabfef870ecc56f344c8c100d
manojgudi <manoj.p.gudi@gmail.com>
2014-05-27 16:55:21 +0530

    Unifinshed implementation

--- dirty_commits/rmq/gr_worker.py
+++ dirty_commits/rmq/gr_worker.py
@@ -1,44 +1,51 @@
 #!/usr/bin/python
 import gras
 import numpy
-import serial 
-class ser(gras.Block):
-	
+import serial
 
-        def __init__(self):
-                gras.Block.__init__(self,
-                        name="ser",
-                        in_sig=[numpy.float32],
-                        out_sig=[numpy.float32])
-		self.n = 1
-        def set_parameters(self, port, baud, bytesize, parity, stopbits):
-		
-		try:
-			print port
-			self.ser_obj = serial.Serial(port, baud,  bytesize, parity,  stopbits)
-			print("serial found on " + port )
-			self.ser_obj.open()
-		except:
-			print "Couldn't Open Serial Port " + port + " Failed"
-	
+import pika
+import cPickle as pickle
 
-        def work(self, input_items, output_items):
-		
-		self.n = input_items[0][0]
-                out = output_items[0][:self.n]
-		# Input is size of output_items to be returned
+from rbf_def import train_rbf, calc_val
 
-		for i in range(self.n):
-			
-			# Try catch block to avoid Error
-			# ValueError: invalid literal for int() with base 10: '\xfe354\r\n'
-			try:
-				out[i] = int(self.ser_obj.readline())
-			except:
-				pass
-		
-		print "OUT", out[:self.n]
-			
-		self.produce(0,len(out)) # Produce from port 0 output_items
-		self.consume(0,1) # Consume from port 0 input_items
+class worker(gras.Block):
+
+    def __init__(self):
+            gras.Block.__init__(self,
+                    name="ser",
+                    in_sig=[numpy.float32],
+                    out_sig=[numpy.float32],
+                    n=1)
+        # window
+        self.n = n
+
+    def create_connection( self, hostname='localhost', queue_name):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(hostname))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue=queue_name)
+
+    def work(self, input_items, output_items):
+
+        ## Internal Functions
+        def callback(self, ch, method, properties, body):
+            self.body = pickle.loads(body)
+            print "Got body"
+
+            # Create Ack
+            ch_basic_ack(delivery_tag = method.delivery_tag)
+            
+            self.trained_rbf = train_rbf(
+
+        self.channel.basic_consume(callback, queue=queue_name, no_ack=False)
+
+        print "Waiting for training_data"
+        self.channel.start_consuming()
+
+        self.n = input_items[0][0]
+        out = output_items[0][:self.n]
+        # Input is size of output_items to be returned
+
+
+        self.produce(0,len(out)) # Produce from port 0 output_items
+        self.consume(0,1) # Consume from port 0 input_items
 
